00a83d0a2ef56205ceb43e29d435fc864da36c6b
mehmy <m.yalvac98@gmail.com>
2023-11-09 10:52:48 +0100

    part 4 initiation

---
+++ part4/bloglist/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUrl =
+  'mongodb+srv://mehmy:Babiirfan.32@cluster0.co8pafm.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
